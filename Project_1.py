2596d75f50c01c8b143f864d02e17c23d8b65aad
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2019-03-19 20:52:17 +0330

    change file name

--- Project_1.py
+++ Project_1.py
@@ -29,10 +29,12 @@ def collect(coins, index, N):
         h_result = h_tail - index + 1
         if v_result > h_result:
             vertical_collect(coins, index)
+            # we go to the next column.
             num_moves, moves = collect(coins, index+1, N)
             return num_moves + 1, f"V {index+1}\n"+moves
         if h_result >= v_result:
             horizontal_collect(coins, index, h_tail)
+            # we stay at current column.
             num_moves, moves = collect(coins, index, N)
             return num_moves+1, f"H {index+1} {h_tail+1}\n"+moves
 
