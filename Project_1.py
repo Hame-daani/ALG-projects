577fdd17d7bb70fbe9aef7de41fa6da22ea379f1
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2019-03-21 02:09:20 +0330

    optimize code

--- Project_1.py
+++ Project_1.py
@@ -16,35 +16,33 @@ def h_pop(coins, low, high, n):
 
 def collect(coins, low, high):
     # base case
-    if low == high:
-        if coins[low]:
-            return 1, f"V {low+1}\n"
     if low > high:
         return 0, ""
 
     # finding minimum height. it's will be our halfing point.
     minimum = find_min(coins, low, high)
+
     # vertical moves number is equal to columns number.
     v_result = high - low + 1
+    v_moves = ""
+    for a in range(low, high+1):
+        v_moves += f"V {a+1}\n"
+
     # horizontal moves number is equal to minimum height that we found.
     h_result = coins[minimum]
-    coins = h_pop(coins, low, high, h_result)
     h_moves = ""
+    coins = h_pop(coins, low, high, h_result)
     for a in range(h_result):
         h_moves += f"H {low+1} {high+1}\n"
 
     left_result, left_moves = collect(coins, low, minimum-1)
     right_result, right_moves = collect(coins, minimum+1, high)
-    
+
     h_result += left_result+right_result
+    h_moves += left_moves + right_moves
     if v_result < h_result:
-        v_moves = ""
-        for a in range(low, high+1):
-            v_moves += f"V {a+1}\n"
         return v_result, v_moves
-    elif h_result <= v_result:
-        h_moves += left_moves
-        h_moves += right_moves
+    else:
         return h_result, h_moves
 
 
@@ -54,7 +52,7 @@ def min_step_collect(coins):
 
 if __name__ == "__main__":
     number_of_columns = 5
-    coins = [2, 1, 2, 5, 1]
+    coins = [2,1,2,5,1]
     num_moves, moves = min_step_collect(coins)
     print(num_moves)
     print(moves)
