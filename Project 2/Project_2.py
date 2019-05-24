5a29eca7e9f36fb5156af20b364308e6d2f97c9f
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2019-05-24 13:14:02 +0430

    add comments to code.

--- Project 2/Project_2.py
+++ Project 2/Project_2.py
@@ -1,4 +1,7 @@
 def findMin(cost):
+    """
+    return minimum between our 5 operation costs.
+    """
     minKey = list(cost.keys())[0]
     minValue = list(cost.values())[0]
     for key, value in cost.items():
@@ -9,20 +12,34 @@ def findMin(cost):
 
 
 def findAnswer(x, y, costs):
+    """
+    main func that return two array for total cost and operations followed to that cost.
+    input:
+        x: source string
+        y: target string
+        costs: dic include each operation's cost.
+    """
+    
+    # initial the arrays
     t_cost = [[0 for j in range(0, len(x)+1)] for i in range(0, len(y)+1)]
     moves = [[0 for j in range(0, len(x)+1)] for i in range(0, len(y)+1)]
     t_cost[0][0] = 0
     moves[0][0] = 0
-    # initial the arrays
+    # fill first row and first column for converting x to null string,and null to y string.
+    # converting x to null: require only delete operation.
+    # converting null to y: is done with insert operation.
     for i in range(1, len(x)+1):
         t_cost[0][i] = i*costs['del']
         moves[0][i] = f"del {x[i-1]}"
     for j in range(1, len(y)+1):
         t_cost[j][0] = j*costs['ins']
         moves[j][0] = f"ins {y[j-1]}"
+    # main loop to fill our two array
     for i in range(1, len(y)+1):
         for j in range(1, len(x)+1):
             cost = {}
+            # we could use copy operaioin only when two character be the same.
+            # otherwise we use replace operation.
             if x[j-1] == y[i-1]:
                 cost['cp'] = costs['cp'] + t_cost[i-1][j-1]
             else:
@@ -33,6 +50,7 @@ def findAnswer(x, y, costs):
                 cost['tw'] = 0
             cost['del'] = costs['del'] + t_cost[i][j-1]
             cost['ins'] = costs['ins'] + t_cost[i-1][j]
+            # find minimum between this operations.
             minKey, minValue = findMin(cost)
             t_cost[i][j] = minValue
             if minKey == 'del' or minKey == 'cp':
@@ -58,6 +76,12 @@ def prettyPrint(x, y, cost, moves):
 
 
 def find(moves, n, m):
+    """
+    return string includes all moves to convert x[0:n] to y[0:m]
+    input:
+        moves: array includes operations that we fill with dynamic programing.
+    """
+    # base case.
     if n == 0 and m == 0:
         return ""
     if moves[n][m].startswith('cp') or moves[n][m].startswith('rep'):
