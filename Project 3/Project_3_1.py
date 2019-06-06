9c2d24c5575a577aa6e8d886b8b70a8118b16748
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2019-06-06 23:37:44 +0430

    3_1 algorithm works

--- Project 3/Project_3_1.py
+++ Project 3/Project_3_1.py
@@ -13,8 +13,39 @@ def readDebts(filename):
                 debts[getter] += int(debt)
             else:
                 debts[getter] = int(debt)
-    return debts
+    return [{'name': key, 'debt': value} for key, value in debts.items()]
+
+
+def itsSolution(debts):
+    if not debts:
+        return True
+    return False
+
+
+def select(debts):
+    return debts[0], debts[-1]
+
+
+def minimum(a, b):
+    return a if a < b else b
 
 
 if __name__ == '__main__':
     debts = readDebts('in.txt')
+    sort_debts = sorted(debts, key=lambda x: x['debt'])
+
+    net_amount = 0
+    # solution check
+    while not itsSolution(sort_debts):
+        giver, getter = select(sort_debts)
+        minDebt = minimum(-1*giver['debt'], getter['debt'])
+        net_amount += minDebt
+        print(f"{giver['name']} --{minDebt}--> {getter['name']}")
+        if minDebt == -1*giver['debt']:
+            sort_debts.pop(0)
+        else:
+            giver['debt'] += minDebt
+        if minDebt == getter['debt']:
+            sort_debts.pop(-1)
+        else:
+            getter['debt'] -= minDebt
