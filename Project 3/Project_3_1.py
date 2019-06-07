0d1c31fae6fa81cab69de9d68a0a9f6508cd3d21
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2019-06-07 19:42:05 +0430

    3_1 3_2 clean up and add comment

--- Project 3/Project_3_1.py
+++ Project 3/Project_3_1.py
@@ -1,46 +1,62 @@
 def readDebts(filename):
+    """
+    read data from file and return a list 
+    include person name and net amout of debt.
+    """
     debts = {}
     with open(filename) as f:
         lines = f.readlines()
         for l in lines:
             l = l.rstrip('\n')
             debt, giver, getter = l.split()
+            # add negative amount for giver
             if giver in debts:
                 debts[giver] -= int(debt)
             else:
                 debts[giver] = -1*int(debt)
+            # add positive amount for getter
             if getter in debts:
                 debts[getter] += int(debt)
             else:
                 debts[getter] = int(debt)
+    # convert to a list include {name,debt}
     return [{'name': key, 'debt': value} for key, value in debts.items()]
 
 
 def itsSolution(debts):
+    """
+    check if we come with a solution or not.
+    """
     if not debts:
         return True
     return False
 
 
 def select(debts):
+    """
+    return min and max debts.
+    """
     return debts[0], debts[-1]
-
-
-def minimum(a, b):
-    return a if a < b else b
 
 
 if __name__ == '__main__':
     debts = readDebts('in.txt')
+    # sorting debts from min to max.
     sort_debts = sorted(debts, key=lambda x: x['debt'])
 
     net_amount = 0
     # solution check
     while not itsSolution(sort_debts):
+        # get giver and getter
         giver, getter = select(sort_debts)
-        minDebt = minimum(-1*giver['debt'], getter['debt'])
+        # find minimum between debt of two.
+        minDebt = min(-1*giver['debt'], getter['debt'])
+        # add it to net amount
         net_amount += minDebt
+        # transit minDebt from giver to getter
         print(f"{giver['name']} --{minDebt}--> {getter['name']}")
+        # subtract min from each debt
+        # or pop it if its equal to min.
         if minDebt == -1*giver['debt']:
             sort_debts.pop(0)
         else:
--- Project 3/Project_3_2.py
+++ Project 3/Project_3_2.py
@@ -2,22 +2,23 @@ squares = {}
 
 
 def cutmin(a, b):
-    # base case
+    # base case: we cut 1 square and we finish the paper.
     if a == b:
         if f"{a}*{b}" in squares:
             squares[f"{a}*{b}"] += 1
         else:
             squares[f"{a}*{b}"] = 1
         return
+    
+    # we only can cut square as size as minimum edge.
     m = min(a, b)
+    # we cut a square with size of m
     if f"{m}*{m}" in squares:
         squares[f"{m}*{m}"] += 1
     else:
         squares[f"{m}*{m}"] = 1
-    if a > b:
-        return cutmin(a-m, b)
-    else:
-        return cutmin(a, b-m)
+    # go recursive to what left from paper.
+    return cutmin(a-m, b) if a > b else cutmin(a, b-m)
 
 
 if __name__ == "__main__":
